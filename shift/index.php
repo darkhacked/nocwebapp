4fe6b8ba0ea392df4a1bfb5493aee236711976cf
darkhacked <godofwarpp@gmail.com>
2020-01-17 17:09:02 +0700

    update shift

---
+++ shift/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+ <link href="css/bootstrap.css" rel="stylesheet">
+ <link href="css/mystyle.css" rel="stylesheet">
+<title>Super</title>
+</head>
+<body>
+ <h1>ssee</h1>
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
